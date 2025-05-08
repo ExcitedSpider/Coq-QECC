b144584a5c1e55bf290b42a111a59ad446cae583
Chew F <qiuyifeng1997@outlook.com>
2025-05-08 18:50:52 +1000

    change to axiom

--- theories/Assumption.v
+++ theories/Assumption.v
@@ -1,18 +1,17 @@
-(* Assumption we made for the formalization *)
+(* Axioms of Linear Algebra *)
 From mathcomp Require Import ssreflect.
 
 Require Import QuantumLib.Quantum.
 
-(* If A is an involutary matrix, A = A^-1 *)
-Lemma involutary_matrix_spec {n}:
-  forall (A: Square (2^n)),
+(* If A is an involutary matrix, A equal to its inverse *)
+(* If A^2 = I then A^-1 = A *)
+Axiom involutary_matrix_spec:
+  forall {n:nat} (A: Square (2^n)),
   A × A = I (2^n) -> A = Minverse A.
-Admitted.
 
 (* For any nonzero matrix, there exists i j that A[i,j] is not 0 *)
-Lemma Mnonzero_spec:
+Axiom Mnonzero_spec:
   forall {n m : nat} (A: Matrix n m),
   WF_Matrix A ->
   A <> Zero -> 
   exists i j , (i < n)%nat /\ (j < m)%nat /\ A i j <> 0.
-Admitted.
